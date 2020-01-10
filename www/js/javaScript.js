68ecede55d5eb09c7d8ff4165f0970bce641ab3f
AndradeLeroy <47416005+AndradeLeroy@users.noreply.github.com>
2020-01-10 11:31:56 +0100

    code optimalizatie
    code optimalizatie

--- www/js/javaScript.js
+++ www/js/javaScript.js
@@ -1,3 +1,23 @@
-let test = document.body.querySelector("aside");
-//test.innerHTML+="<img src='https://via.placeholder.com/360'>".style.height="180";
-test.innerHTML+="<img src="+ 'https://via.placeholder.com/460'+ ">
+
+//test.innerHTML+="<img src='https://via.placeholder.com/360'>".style.height="180"
+//test.innerHTML+="<img src="+ 'https://via.placeholder.com/460'+ ">
+//string concationation
+
+
+let generator=["../splash/960x1960.png"];
+//  "<img src="+'https://via.placeholder.com/960x1960'+">", "<img src="+'https://via.placeholder.com/960x1960'+">","<img src="+'https://via.placeholder.com/960x1960'+">"];
+/*
+                "<img src="+'https://via.placeholder.com/960x1960'+">","<img src="+'https://via.placeholder.com/960x1960'+">",
+                "<img src="+'https://via.placeholder.com/960x1960'+">","<img src="+'https://via.placeholder.com/960x1960'+">",
+                "<img src="+'https://via.placeholder.com/960x1960'+">","<img src="+'https://via.placeholder.com/960x1960'+">",
+                "<img src="+'https://via.placeholder.com/960x1960'+">","<img src="+'https://via.placeholder.com/960x1960'+">",
+                "<img src="+'https://via.placeholder.com/960x1960'+">","<img src="+'https://via.placeholder.com/960x1960'+">",
+                "<img src="+'https://via.placeholder.com/960x1960'+">"]
+*/
+for (let i=0; i<=12; i++)
+{
+  document.write("<img src="+'generator[i]'"+"</img>");
+//  let test+=document.getElementById("wrapper").innerHTML=text;
+}
+
+// let test = document.getElementById("wrapper").innerHTML=generator[0];
