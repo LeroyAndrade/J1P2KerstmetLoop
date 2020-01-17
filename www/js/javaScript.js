2a5fd67ee03e8dd5c5608a1c8b398cfc072e2f59
AndradeLeroy <47416005+AndradeLeroy@users.noreply.github.com>
2020-01-17 15:17:54 +0100

    Afbeelding met een halve MB weten te reduceren en alle bestanden als zip bijgevoegd
    Notitie is dat de HTML code met twee items minder kan worden geschreven. Dit staat op de todo lijst

--- www/js/javaScript.js
+++ www/js/javaScript.js
@@ -1,7 +1,19 @@
+
+//test.innerHTML+="<img src='https://via.placeholder.com/360'>".style.height="180"
+//test.innerHTML+="<img src="+ 'https://via.placeholder.com/460'+ ">
+//string concationation
+
+
 let generator=["./splash/960x1960"];
 
 let i =3;
 for(; i<13; i++)
 {
   document.getElementById("wrapper").innerHTML+= "<img id="+'grid'+ i +" src=" +generator[0]+ i +".png"+" width="+'100%'+"+ height="+'100%'+">";
+
 }
+
+
+
+
+// let test = document.getElementById("wrapper").innerHTML=generator[0];
