c27a11f02da406f67057b53b3d32d0f0e23c16db
AndradeLeroy <47416005+AndradeLeroy@users.noreply.github.com>
2020-01-17 15:01:33 +0100

    Hotfix opdracht afgerondt
    Alle punten zijn uitgevoerd, waardoor het een tien zou moeten worden

--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
+Inloggen tot server tijdelijk offline tot servicedesk het probleem heeft opgelost.
 "https://github.com/LeroyAndrade/J1P2KerstmetLoop" 
--- www/js/javaScript.js
+++ www/js/javaScript.js
@@ -1,23 +1,7 @@
+let generator=["./splash/960x1960"];
 
-//test.innerHTML+="<img src='https://via.placeholder.com/360'>".style.height="180"
-//test.innerHTML+="<img src="+ 'https://via.placeholder.com/460'+ ">
-//string concationation
-
-
-let generator=["../splash/960x1960.png"];
-//  "<img src="+'https://via.placeholder.com/960x1960'+">", "<img src="+'https://via.placeholder.com/960x1960'+">","<img src="+'https://via.placeholder.com/960x1960'+">"];
-/*
-                "<img src="+'https://via.placeholder.com/960x1960'+">","<img src="+'https://via.placeholder.com/960x1960'+">",
-                "<img src="+'https://via.placeholder.com/960x1960'+">","<img src="+'https://via.placeholder.com/960x1960'+">",
-                "<img src="+'https://via.placeholder.com/960x1960'+">","<img src="+'https://via.placeholder.com/960x1960'+">",
-                "<img src="+'https://via.placeholder.com/960x1960'+">","<img src="+'https://via.placeholder.com/960x1960'+">",
-                "<img src="+'https://via.placeholder.com/960x1960'+">","<img src="+'https://via.placeholder.com/960x1960'+">",
-                "<img src="+'https://via.placeholder.com/960x1960'+">"]
-*/
-for (let i=0; i<=12; i++)
+let i =3;
+for(; i<13; i++)
 {
-  document.write("<img src="+'generator[i]'"+"</img>");
-//  let test+=document.getElementById("wrapper").innerHTML=text;
+  document.getElementById("wrapper").innerHTML+= "<img id="+'grid'+ i +" src=" +generator[0]+ i +".png"+" width="+'100%'+"+ height="+'100%'+">";
 }
-
-// let test = document.getElementById("wrapper").innerHTML=generator[0];
