9920c0a1c5e3a5f08050644ea46867f90b45a6ce
AndradeLeroy <47416005+AndradeLeroy@users.noreply.github.com>
2020-01-20 11:49:33 +0100

    Push na advies docent

--- www/js/javaScript.js
+++ www/js/javaScript.js
@@ -9,8 +9,7 @@ let generator=["./splash/960x1960"];
 let i =3;
 for(; i<13; i++)
 {
-  document.getElementById("wrapper").innerHTML+= "<img id="+'grid'+ i +" src=" +generator[0]+ i +".png"+" width="+'100%'+"+ height="+'100%'+">";
-
+  document.getElementById("wrapper").innerHTML+= "<img id="+'grid'+ i +" src=" +generator[0]+ i +".png"+">";
 }
 
 
